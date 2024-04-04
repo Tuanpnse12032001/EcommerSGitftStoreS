c8c2ec5a223df8ce4e26b92e6cd133a5bef073d3
Tuanpnse12032001 <tuanpn12301@gmail.com>
2024-04-04 10:56:42 +0700

    commit 2

---
+++ src/main/java/org/example/ecommersgitftstores/EcommerSGitftStoreSApplication.java
@@ -0,0 +1,13 @@
+package org.example.ecommersgitftstores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerSGitftStoreSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerSGitftStoreSApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/ecommersgitftstores/EcommerSGitftStoreSApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ecommersgitftstores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerSGitftStoreSApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
