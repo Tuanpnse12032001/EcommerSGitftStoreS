3e096d6991866a4fa5d6dd860cb1f9b522cf1b28
Tuanpnse12032001 <tuanpn12301@gmail.com>
2024-04-04 11:02:46 +0700

    Start Config push

---
+++ src/main/java/security/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package security;
+
+import com.gifthommie.backend.entity.User;
+import com.gifthommie.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) 
+			throws UsernameNotFoundException {
+		User user = userRepository.getUserByUsernameOrEmail(username);
+		
+		if (user == null) {
+			throw new UsernameNotFoundException("Could not find user");
+		}
+		
+		return new MyUserDetails(user);
+	}
+
+}
